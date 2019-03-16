cb000d796e93265cdb8190112adc9a8a335365b9
kempul <haidarfaz@gmail.com>
2019-03-16 20:31:06 +0700

    initial

---
+++ AntrianServerProject1.dpr
@@ -0,0 +1,15 @@
+program AntrianServerProject1;
+
+uses
+  Vcl.Forms,
+  AntrianServerUnit1 in 'AntrianServerUnit1.pas' {Form1},
+  antrikanU in 'antrikanU.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
